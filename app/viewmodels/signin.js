b4574b7aa97c555212f8fd0d0a6068847e4edbb1
towtow <tobias.wildgruber71@googlemail.com>
2014-11-06 02:29:35 +0100

    auth woes

--- app/viewmodels/signin.js
+++ app/viewmodels/signin.js
@@ -1,10 +1,16 @@
 ﻿define(['plugins/http', 'durandal/app', 'knockout', 'viewmodels/shell'], function (http, app, ko, shell) {
     var model = {
+        username: ko.observable(),
+        password: ko.observable(),
         signin: function signin() {
-            console.log("asdasd");
-            http.get('https://ng.conject.com/rest').done(function (result) {
-                shell.onSignIn(result);
-            });
+            http.post('https://ng.conject.com/ng/loginController.do?actionId=LoginForm.login&userNameTextBox_currentValue=' +
+                encodeURIComponent(model.username()) + '&passwordTextBox_currentValue=' + encodeURIComponent(model.password()) +
+                '&fname=topWindow&finitiat=topWindow&NG-FORM-VIEW-ID=/login/loginPage.xhtml&NG-FORM-MAPPING=/loginController').done(function () {
+                    http.get('https://ng.conject.com/rest').done(function (result) {
+                        shell.onSignIn(result);
+                    });
+                }
+            );
         }
     };
     return model;
